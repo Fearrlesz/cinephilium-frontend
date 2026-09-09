3445b0c190a5b54561087186b0716d5fae2013d2
Fearrlesz <borodinnikita963@gmail.com>
2026-09-09 15:36:51 +0300

    Create NotificationContext.js

---
+++ context/NotificationContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useContext } from 'react';
+
+const NotificationContext = createContext();
+
+export function useNotification() {
+  const context = useContext(NotificationContext);
+  if (!context) {
+    throw new Error('useNotification must be used within NotificationProvider');
+  }
+  return context;
+}
+
+export default NotificationContext;
