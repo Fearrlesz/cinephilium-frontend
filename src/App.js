84471bb8d7f110d94bfb638c21161d0e64d5947a
Fearrlesz <borodinnikita963@gmail.com>
2026-09-10 17:58:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotificationContext from './context/NotificationContext';
 import NotificationModal from './components/NotificationModal';
