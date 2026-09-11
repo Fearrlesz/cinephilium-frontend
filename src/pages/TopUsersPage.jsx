bbd29a9c1625f0f42732bcae9fa19b134506e40a
Fearrlesz <borodinnikita963@gmail.com>
2026-09-12 01:31:37 +0300

    Update TopUsersPage.jsx

--- src/pages/TopUsersPage.jsx
+++ src/pages/TopUsersPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../api/client';
+import './TopUsersPage.css';
 
 function TopUsersPage() {
   const [users, setUsers] = useState([]);
