2e807b43276fe7cbb426b6356cff0e896f387fe1
Fearrlesz <borodinnikita963@gmail.com>
2026-09-12 01:37:03 +0300

    Update ProfilePage.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -6,6 +6,7 @@ import useActivityEvents from '../hooks/useActivityEvents';
 import { formatDate } from '../utils/ratingUtils';
 import { getScoreColor } from '../utils/constants';
 import RatingDetailsModal from '../components/RatingDetailsModal';
+import './ProfilePage.css';
 
 function ProfilePage() {
   const [user, setUser] = useState(null);
