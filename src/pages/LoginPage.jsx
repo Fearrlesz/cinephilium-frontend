f91c556398f7c17cfb5f614b1511b6d68d1d1bc9
Fearrlesz <borodinnikita963@gmail.com>
2026-09-12 01:22:32 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import api from '../api/client';
 import { useNotification } from '../context/NotificationContext';
 import { validateEmail, validatePassword, validateNickname } from '../utils/ratingUtils';
+import './LoginPage.css';
 
 function LoginPage() {
   const [isLogin, setIsLogin] = useState(true);
