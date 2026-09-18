a20e398dbc32a08bc8be8c5dd44fac7cbc53baba
Fearrlesz <borodinnikita963@gmail.com>
2026-09-19 02:11:03 +0300

    Update UserProfilePage.jsx

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -7,6 +7,7 @@ import { getScoreColor } from '../utils/constants';
 import RatingDetailsModal from '../components/RatingDetailsModal';
 import './ProfilePage.css';
 import './UserProfilePage.css';
+import './Architect.css';
 
 function UserProfilePage() {
   const { id } = useParams();
